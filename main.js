afa1d83825698ef5e56f8ca436d770f9f5fbe5d1
Jose Ibanez <jocibanez1904@gmail.com>
2024-05-18 17:05:54 -0500

    Ajustar la etiqueta meta para evitar errores visuales

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var backgroundColor;
 
+    // Definir los colores de fondo según la posición de desplazamiento
+    if (scrollPosition < 500) {
+      backgroundColor = 'blue';
+    } else if (scrollPosition < 1000) {
+      backgroundColor = 'green';
+    } else {
+      backgroundColor = 'red';
+    }
+
+    // Aplicar el color de fondo al elemento con la clase "content"
+    document.querySelector('.content').style.backgroundColor = backgroundColor;
+  });
