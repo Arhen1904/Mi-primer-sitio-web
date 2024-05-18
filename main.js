a65ff0b55ba33401d49c8532fd5dfda0a83d6f60
Jose Ibanez <jocibanez1904@gmail.com>
2024-05-18 18:03:47 -0500

    Ajustar la visualización para navegadores móviles

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var backgroundColor;
-
-    // Definir los colores de fondo según la posición de desplazamiento
-    if (scrollPosition < 500) {
-      backgroundColor = 'blue';
-    } else if (scrollPosition < 1000) {
-      backgroundColor = 'green';
-    } else {
-      backgroundColor = 'red';
-    }
-
-    // Aplicar el color de fondo al elemento con la clase "content"
-    document.querySelector('.content').style.backgroundColor = backgroundColor;
+ScrollTrigger.config({
+    autoRefreshEvents: "visibilitychange,DOMContentLoaded,load" // notice "resize" isn't in the list
   });
